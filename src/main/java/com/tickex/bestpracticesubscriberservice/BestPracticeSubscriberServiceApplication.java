268e2f881c3d3d01ab9a263e4a6403a888881e23
Your Name <you@example.com>
2026-01-20 13:55:14 +0300

    init

---
+++ src/main/java/com/tickex/bestpracticesubscriberservice/BestPracticeSubscriberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tickex.bestpracticesubscriberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPracticeSubscriberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestPracticeSubscriberServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickex/bestpracticesubscriberservice/BestPracticeSubscriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickex.bestpracticesubscriberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPracticeSubscriberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
